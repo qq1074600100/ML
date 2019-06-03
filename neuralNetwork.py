98f136a46ea27738b6bec85e86bf784131d7ff90
maple_mrzhao <mrzhao@mapleleaf.com>
2019-06-04 00:45:58 +0800

    fix: fix some bug of neuralNetwork.py,       it is because it use different data dim with others

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -4,33 +4,7 @@ from sklearn import datasets, linear_model
 import matplotlib.pyplot as plt
 
 
-def generate_data():
-    np.random.seed(0)
-    X, y = datasets.make_moons(200, noise=0.20)
-    # print(len(y))
-    return X, y
-
-
-def plot_decision_boundary(pred_func, X, y):
-    # Set min and max values and give it some padding
-    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-    h = 0.01
-    # Generate a grid of points with distance h between them
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-    # Predict the function value for the whole gid
-    temp = np.c_[xx.ravel(), yy.ravel()]
-    Z = pred_func(temp.T)
-    Z = Z.reshape(xx.shape)
-    # Plot the contour and training examples
-    plt.contourf(xx, yy, Z)
-    plt.scatter(X[:, 0], X[:, 1], c=y)
-    plt.show()
-
 # 使用logistic函数作为激活函数的神经网络，三层
-
-
 class NeuralNetwork(object):
     def __init__(self, numInput, numOutput,
                  numHide=3, step=0.01, regLambda=0.01):
@@ -166,3 +140,20 @@ class NeuralNetwork(object):
         z2 = W2.dot(a1)
         probs = self.calLogisticFunc(z2)
         return np.argmax(probs, axis=0)
+
+    def plot_decision_boundary(self, X, y):
+        # Set min and max values and give it some padding
+        x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+        y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+        h = 0.01
+        # Generate a grid of points with distance h between them
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                             np.arange(y_min, y_max, h))
+        # Predict the function value for the whole gid
+        temp = np.c_[xx.ravel(), yy.ravel()]
+        Z = self.predict(temp.T)
+        Z = Z.reshape(xx.shape)
+        # Plot the contour and training examples
+        plt.contourf(xx, yy, Z)
+        plt.scatter(X[:, 0], X[:, 1], c=y)
+        plt.show()
