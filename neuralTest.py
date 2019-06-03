16a48d8a252e03e585f45ea2d87b25af28f7c68a
maple_mrzhao <mrzhao@mapleleaf.com>
2019-06-04 00:49:30 +0800

    commit: my test file for neural network

--- neuralTest.py
+++ neuralTest.py
@@ -1,8 +1,10 @@
-import neuralNetwork
 import numpy as np
 from sklearn import datasets, linear_model
 import matplotlib.pyplot as plt
-
+import neuralNetworkUtils as utils
+from neuralNetworkVectorize import NeuralNetworkVectorize
+from neuralNetwork import NeuralNetwork
+from neuralNetworkGeneric import NeuralNetworkGeneric
 
 # def f(x):
 #     rst = x.dot(np.array([[0.3], [-1]], dtype='float64'))
@@ -14,11 +16,15 @@ import matplotlib.pyplot as plt
 #     return rst
 
 
-model = neuralNetwork.NeuralNetwork(2, 2, numHide=3, step=1,regLambda=0.01)
+X, y = utils.generate_data()
+Y = np.zeros((len(y), 2), dtype='float64')
+for i in range(len(y)):
+    Y[i, y[i]] = 1
+layerNodes = [2, 3, 3, 2]
+# model = NeuralNetwork(2, 2, numHide=3, step=1, regLambda=0.01)
+# model = NeuralNetworkVectorize(X, Y, numHide=3, step=1, regLambda=0.001)
+model = NeuralNetworkGeneric(X, Y, layerNodes, step=1, regLambda=0.001)
 model.calModel()
 
-
-X, y = neuralNetwork.generate_data()
-# plt.scatter(X[:, 0], X[:, 1], s=40, c=y)
-neuralNetwork.plot_decision_boundary(
-    lambda x: model.predict(x), X, y)
+utils.plot_decision_boundary(lambda x: model.predict(x), X, y)
+# model.plot_decision_boundary(X, y)
